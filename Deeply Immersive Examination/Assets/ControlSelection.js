d38b477c90feb90cda86839ebb9a89564b8c0141
Jonny van der Hoeven <jonnyhoeven@gmail.com>
2015-11-15 10:59:27 +0100

    Eduhack 2015
    One night for better immersive examination methods,

---
+++ Deeply Immersive Examination/Assets/ControlSelection.js
@@ -0,0 +1,42 @@
+﻿var selectiontime = 0.0;
+var linkToScriptB : MoveNext;
+var soundplayed: boolean;
+var timelastact = 0.0;
+
+
+function Start() {
+    soundplayed = false;
+}
+
+
+function Update(){
+    if (selectiontime >0) {
+      //  selectiontime = selectiontime - (Time.deltaTime);
+       // print(selectiontime);
+        transform.Rotate (0,selectiontime*6,0);
+
+    }
+ }
+
+function Highlite () {
+    timelastact = Time.time;
+    selectiontime += Time.deltaTime * 1;
+    if (selectiontime > 12) {
+        print("Keuze gemaakt");
+
+        if (!soundplayed) {
+        
+            soundplayed = true;
+            var audio: AudioSource = GetComponent.<AudioSource>();
+            audio.Play();
+            audio.Play(44100);
+
+        }
+
+        transform.Rotate(0,0,0);
+        linkToScriptB = GetComponent(MoveNext);
+        linkToScriptB.MoveOn();
+    } else {
+        transform.Rotate (0,selectiontime* 4,0);
+    }
+}
---
+++ Deeply Immersive Examination/Assets/resources/CameraRayCollisionUpdateLoop.js
@@ -0,0 +1,15 @@
+﻿var hit : RaycastHit;
+var target: GameObject;
+var linkToScriptB : ControlSelection;
+
+
+function Update() {
+    var fwd = transform.TransformDirection (Vector3.forward);
+    if (Physics.Raycast (transform.position, fwd, hit)) {
+        target = hit.collider.transform.gameObject;
+
+        linkToScriptB = target.GetComponent(ControlSelection);
+        linkToScriptB.Highlite();
+        
+    }
+}
